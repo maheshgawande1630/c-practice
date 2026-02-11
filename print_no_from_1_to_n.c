5d98442834c9569a6bf800d9abfab91df2248951
Mahesh Gawande <maheshgawande681@gmail.com>
2026-02-11 23:53:51 +0530

    print no using loop

---
+++ print_no_from_1_to_n.c
@@ -0,0 +1,15 @@
+//using for loop
+#include<stdio.h>
+
+int main(){
+
+    int n,i;
+    printf("Enter no");
+    scanf("%d", &n);
+
+    for(i=1 ; i<=n; i++){
+
+        printf("%d\n", i);
+        return 0;
+    }
+}
