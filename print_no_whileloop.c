3909d0c35d5d6e4bdd63019a2a38ff674556a668
Mahesh Gawande <maheshgawande681@gmail.com>
2026-02-13 19:26:17 +0530

    print no using while loop

---
+++ print_no_whileloop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main (){
+
+    int m;
+    int n=0;
+   
+
+    printf("enter no");
+    scanf("%d", &m);
+
+while(n<=m){
+
+printf("%d", n);
+ n++;
+
+}
+
+return 0;
+
+}
