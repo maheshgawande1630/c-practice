79df4acf4c2feb028be6ae2b5e04727e73d14bd6
Mahesh Gawande <maheshgawande681@gmail.com>
2026-02-15 21:26:56 +0530

    print fibonacci series

---
+++ Print_Fibonacci_series.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int n, a = 0, b = 1, c;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+ 
+    printf("Fibonacci series up to %d is:\n", n);
+
+    for(int i = 0; a <= n; i++){
+    
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+
+                  }
+                 
+
+    return 0;
+
+    }
